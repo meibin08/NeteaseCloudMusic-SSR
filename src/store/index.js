2447041f691f3affa3866b07c06f98f371b30f60
meibin08 <meiguangbin001@zhongan.com>
2018-07-19 11:00:20 +0800

    add

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,15 @@ import {autoRehydrate} from 'redux-persist'
 import reducers from "./reducers";
 
 function configStore (initState={}){
-    let createStoreWithMiddleware = compose(applyMiddleware(thunk),autoRehydrate(),(__DEBUG__&& __CLIENT__ && window.devToolsExtension ? window.devToolsExtension() : undefined));
+
+		let createStoreWithMiddleware = compose(applyMiddleware(thunk),autoRehydrate())
+		return createStore(reducers,initState,createStoreWithMiddleware,(__DEBUG__&& __CLIENT__ && window.devToolsExtension ? window.devToolsExtension() : undefined) );
+    
     //dev环境开启redux调试
-    return createStore(reducers,initState,createStoreWithMiddleware);
+    /*let createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+    	return createStoreWithMiddleware(reducers,initState,(__DEBUG__&& __CLIENT__ && window.devToolsExtension ? window.devToolsExtension() : undefined));*/
 };
 
 export default configStore;
+
+
